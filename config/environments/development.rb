c08a8099bd4b33314eb9da9653b4dc91b1e3aea7
Pranav Verma <pranavverma1295@hotmail.com>
2020-05-01 18:14:20 +0100

    update

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,6 +63,14 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { :address => '127.0.0.1', :port => 1025 }
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'website.com',
+    user_name:            'user@website.com',
+    password:             'password',
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
 end
